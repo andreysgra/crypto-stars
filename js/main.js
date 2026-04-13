77154092108a98a10d22dd0134f54b0ade458d54
Andrey Grachev <andreysgra@gmail.com>
2026-04-13 21:34:17 +0300

    Создаёт модуль для отображения информации о пользователе.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 import {getData} from './api.js';
 import {BASE_URL} from './const.js';
 import {renderContractors} from './contractors.js';
+import {renderUser} from './user.js';
 
 (async () => {
   const contractors = await getData(`${BASE_URL}/contractors`);
   renderContractors(contractors);
+
+  const user = await getData(`${BASE_URL}/user`);
+  renderUser(user);
 })();
